dd0f625a30a133a30afbf8d16a35f3ea89d88886
Akshay Shine Krishna <akshayshinekrishna@gmail.com>
2026-01-07 10:20:46 +0530

    updated main runner

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,9 +1,29 @@
 package com.demo;
 
+import com.demo.models.Cards;
+import com.demo.services.Deck;
+
+import java.util.ArrayList;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class Main {
     public static void main(String[] args) {
-       for(int i = 0; i > 5; i++){
-        System.out.println(i);
-       }
+        Deck deck = new Deck();
+        deck.shuffle();
+        List<Cards> drawnCards = new ArrayList<>();
+        for(int i = 0; i<20 ; i++){
+            try {
+                drawnCards.add(deck.drawCard());
+            } catch (NoSuchElementException e) {
+                System.out.println("NoSuchElementException : " + e.getMessage());
+                break;
+            }
+        }
+        System.out.println("\nDrawn cards : \n" + drawnCards);
+        drawnCards.sort(Comparator.comparing(Cards::getSuit).thenComparing(Cards::getRank));
+        System.out.println("\nDrawn cards after sorting based on suit then based on rank : \n" + drawnCards);
     }
 }
